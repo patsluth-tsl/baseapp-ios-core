f9e97a4bde66545ff0dac5610fd06b33f1f8c779
Pat Sluth <ps@tsl.io>
2020-04-27 12:41:40 -0600

    0.1.9

---
+++ baseapp-ios-core-v1/src/Default/Classes/UIKit/UIBarButtonItem+BA.swift
@@ -0,0 +1,45 @@
+//
+//  UICollectionView.swift
+//  baseapp-ios-core-v1
+//
+//  Created by Pat Sluth on 2017-12-19.
+//  Copyright © 2017 Pat Sluth. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - Init Helpers
+public extension UIBarButtonItem {
+    convenience init(image: UIImage?,
+                     style: UIBarButtonItem.Style) {
+        self.init(image: image,
+                  style: style,
+                  target: nil,
+                  action: nil)
+    }
+    
+    @available(iOS 5.0, *)
+    convenience init(image: UIImage?,
+                     landscapeImagePhone: UIImage?,
+                     style: UIBarButtonItem.Style) {
+        self.init(image: image,
+                  landscapeImagePhone: landscapeImagePhone,
+                  style: style,
+                  target: nil,
+                  action: nil)
+    }
+    
+    convenience init(title: String?,
+                     style: UIBarButtonItem.Style) {
+        self.init(title: title,
+                  style: style,
+                  target: nil,
+                  action: nil)
+    }
+    
+    convenience init(systemItem: UIBarButtonItem.SystemItem) {
+        self.init(barButtonSystemItem: systemItem,
+                  target: nil,
+                  action: nil)
+    }
+}
