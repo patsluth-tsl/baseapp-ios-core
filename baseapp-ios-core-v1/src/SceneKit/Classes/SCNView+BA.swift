e7dc3c62be1baccda7f485fc97f00b3bf959cf6b
Patrick Sluth <ps@tsl.io>
2021-05-31 18:49:28 -0600

    0.2.16

---
+++ baseapp-ios-core-v1/src/SceneKit/Classes/SCNView+BA.swift
@@ -0,0 +1,42 @@
+//
+//  SCNView+BA.swift
+//  baseapp-ios-core-v1
+//
+//  Created by Pat Sluth on 2017-10-18.
+//  Copyright © 2017 Apple. All rights reserved.
+//
+
+import Foundation
+import SceneKit
+
+public extension SCNView {
+    /// Return the `pointOfView` or fatalError
+    var pointOfViewUnsafe: SCNNode {
+        guard let pov = pointOfView else {
+            fatalError("Expected a valid `pointOfView` from the scene.")
+        }
+        return pov
+    }
+    
+    /// Return the `pointOfView.camera` or fatalError
+    var cameraUnsafe: SCNCamera {
+        guard let camera = pointOfViewUnsafe.camera else {
+            fatalError("Expected a valid `camera` from the scene.")
+        }
+        return camera
+    }
+    
+    ///
+    /// Safe access to cached `bounds` on any thread.
+    ///
+    var boundsThreadSafe: CGRect {
+        if Thread.isMainThread {
+            set(associatedObject: "boundsThreadSafe", object: bounds, policy: .OBJC_ASSOCIATION_COPY)
+            return bounds
+        }
+        DispatchQueue.main.async(execute: { [weak self] in
+            _ = self?.boundsThreadSafe
+        })
+        return self.get(associatedObject: "boundsThreadSafe", CGRect.self) ?? CGRect.null
+    }
+}
