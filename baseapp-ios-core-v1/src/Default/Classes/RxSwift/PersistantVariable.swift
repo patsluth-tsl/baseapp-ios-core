35a3d2cc3c1aaca020f44e256857166c420524af
Patrick Sluth <ps@tsl.io>
2021-09-28 12:50:18 -0600

    0.2.31

--- baseapp-ios-core-v1/src/Default/Classes/RxSwift/PersistantVariable.swift
+++ baseapp-ios-core-v1/src/Default/Classes/RxSwift/PersistantVariable.swift
@@ -10,58 +10,137 @@ import Foundation
 import RxCocoa
 import RxSwift
 
-/// A BehaviorRelay wrapper that saves and reads value from UserDefaults
+/// A `BehaviorRelay` wrapper that saves and reads value from `UserDefaults`
 public class PersistantVariable<T>: ObservableType {
     // swiftlint:disable:next type_name
-	public typealias E = T?
+    public typealias E = T
+    public typealias Serializer = PersistantVariableValueSerializer<T>
     
-	let key: String
-	let userDefaults: UserDefaults
-	private let _relay: BehaviorRelay<E>
-	
-	public var value: Element {
-		get {
-			return _relay.value
-		}
-		set {
-			accept(newValue)
-		}
-	}
+    let key: String
+    let defaultValue: Element
+    let serializer: Serializer
+    let userDefaults: UserDefaults
     
-    public init(_ type: T.Type,
-                key _key: String,
-                userDefaults _userDefaults: UserDefaults = UserDefaults.standard) {
-        key = _key
-        userDefaults = _userDefaults
-        _relay = BehaviorRelay(value: nil)
-        
-        defer {
-            value = readValue()
+    private let _relay = BehaviorRelay<Element?>(value: nil)
+    
+    public var value: Element {
+        get {
+            return _relay.value ?? defaultValue
+        }
+        set {
+            accept(newValue)
         }
     }
-	
-	func readValue() -> Element {
-		let value = userDefaults.value(forKey: key) as? T
-		return value
-	}
-	
-	func write(value: Element) {
-        userDefaults.setValue(value, forKey: key)
-	}
-	
-	public func accept(_ event: E) {
-        _relay.accept(event)
-		
-		write(value: value)
-	}
-	
-	// MARK: ObservableType
-	public func subscribe<O>(_ observer: O) -> Disposable
-		where O: ObserverType, O.Element == Element {
-		return _relay.subscribe(observer)
-	}
-	
-	public func asObservable() -> Observable<E> {
-		return _relay.asObservable()
-	}
+    
+    public init(_ type: T.Type,
+                key: String,
+                defaultValue: T,
+                serializer: Serializer,
+                userDefaults: UserDefaults = UserDefaults.standard) {
+        self.key = key
+        self.defaultValue = defaultValue
+        self.serializer = serializer
+        self.userDefaults = userDefaults
+        defer {
+            value = (try? readValue()) ?? defaultValue
+        }
+    }
+    
+    func readValue() throws -> Element {
+        guard let value = userDefaults.value(forKey: key) else { return defaultValue }
+        return try serializer.read(value)
+    }
+    
+    func write(value: Element) throws {
+        userDefaults.setValue(try serializer.write(value), forKey: key)
+    }
+    
+    public func accept(_ event: E) {
+        do {
+            try write(value: event)
+            _relay.accept(event)
+        } catch {
+            logger.error(error)
+        }
+    }
+    
+    
+    // MARK: ObservableType
+    public func subscribe<O>(_ observer: O) -> Disposable
+    where O: ObserverType, O.Element == Element {
+        return asObservable().subscribe(observer)
+    }
+    
+    public func asObservable() -> Observable<E> {
+        return _relay.asObservable().map({ [unowned self] in
+            $0 ?? self.defaultValue
+        })
+    }
+}
+
+
+public final class PersistantVariableValueSerializer<T> {
+    public let read: (Any) throws -> T
+    public let write: (T) throws -> Any
+    
+    init(read: @escaping (Any) throws -> T, write: @escaping (T) throws -> Any) {
+        self.read = read
+        self.write = write
+    }
+}
+
+public extension PersistantVariableValueSerializer
+where T: RawRepresentable {
+    convenience init() {
+        self.init(read: {
+            guard let rawValue = $0 as? T.RawValue, let value = T(rawValue: rawValue)
+            else {
+                throw Errors.Decoding(T.self, codingPath: [])
+            }
+            return value
+        }, write: {
+            return $0.rawValue
+        })
+    }
+}
+
+public extension PersistantVariableValueSerializer
+where T: Codable {
+    convenience init<R>(rawType: R.Type) {
+        self.init(read: {
+            return try T.decode($0)
+        }, write: {
+            return try $0.encode(R.self)
+        })
+    }
+}
+
+
+/// A property wrapper for `PersistantVariable`
+@propertyWrapper public final class Persistant<T>: PersistantVariable<T> {
+    public var projectedValue: Observable<E> {
+        return asObservable()
+    }
+    
+    public var wrappedValue: E {
+        get {
+            return value ?? defaultValue
+        }
+        set {
+            value = newValue
+        }
+    }
+    
+    public init(wrappedValue: E,
+                key: String,
+                serializer: Serializer,
+                userDefaults: UserDefaults = UserDefaults.standard) {
+        super.init(
+            T.self,
+            key: key,
+            defaultValue: wrappedValue,
+            serializer: serializer,
+            userDefaults: userDefaults
+        )
+    }
 }
