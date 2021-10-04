e2ca1884aa8e75b543c57cb7cd390e37532adb84
Patrick Sluth <ps@tsl.io>
2021-10-04 15:14:50 -0600

    0.2.34

--- baseapp-ios-core-v1/src/Default/Classes/Foundation/Measurement+BA.swift
+++ baseapp-ios-core-v1/src/Default/Classes/Foundation/Measurement+BA.swift
@@ -15,3 +15,16 @@ public func floor<U: Unit>(_ m: Measurement<U>) -> Measurement<U> {
 public func ceil<U: Unit>(_ m: Measurement<U>) -> Measurement<U> {
     return Measurement<U>(value: Darwin.ceil(m.value), unit: m.unit)
 }
+
+public extension Measurement where UnitType: Dimension {
+    func rounded(toNearest unit: UnitType) -> Measurement<UnitType> {
+        var _measurement = self.converted(to: unit)
+        _measurement.value = Darwin.round(_measurement.value)
+        _measurement.convert(to: self.unit)
+        return _measurement
+    }
+    
+    mutating func round(toNearest unit: UnitType) {
+        self = self.rounded(toNearest: unit)
+    }
+}
