7c7b446b794381340926e63ceb293b4f131acf5c
Patrick Sluth <ps@tsl.io>
2022-02-18 13:18:48 -0700

    0.2.39

--- baseapp-ios-core-v1/src/simd/Classes/simd_float4x4.swift
+++ baseapp-ios-core-v1/src/simd/Classes/simd_float4x4.swift
@@ -40,6 +40,15 @@ public extension simd_float4x4 {
         return simd_quatf(self)
     }
     
+    // TODO: Add to SCNMatrix4 extension
+    var eulerAngles: simd_float3 {
+        simd_float3(
+            x: asin(-self[2][1]),
+            y: atan2(self[2][0], self[2][2]),
+            z: atan2(self[0][1], self[1][1])
+        )
+    }
+    
     var scale: simd_float3 {
         return simd_float3(
             simd_float3(m11, m12, m13).length,
